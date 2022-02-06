bbea90a0f46c0492bd99cd5b80b8a368463be3fd
Helen <le.helen19@gmail.com>
2022-02-06 13:29:56 -0800

    Created Page, PageRange, Table, Database (Data Model)

---
+++ lstore/page.py
@@ -0,0 +1,26 @@
+
+class Page:
+
+    def __init__(self,index, Page_Range):
+        self.num_records = 0
+        self.data = bytearray(4096)
+        self.index = index
+        self.Page_Range= Page_Range
+        Page_Range.pages.append(self)
+
+    def has_capacity(self):
+        if self.num_records >=512:
+            return False
+        return True
+    
+
+    def write(self, value):
+        self.num_records += 1
+        pass
+
+class Page_Range:
+
+    def __init__(self,index,Table):
+        self.pages = []
+        self.index = index
+        Table.page_directory[self] = self.pages
