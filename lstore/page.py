ad3560a4b2d6c16eb46d921d30db79b665892380
Helen <le.helen19@gmail.com>
2022-02-06 14:04:56 -0800

    Resolving more extra text

--- lstore/page.py
+++ lstore/page.py
@@ -1,12 +1,6 @@
 
 class Page:
 
-    def __init__(self):
-        self.num_records = 0
-        self.data = bytearray(4096)
-
-    def has_capacity(self):
-        pass
     def __init__(self,index, Page_Range):
         self.num_records = 0
         self.data = bytearray(4096)
@@ -18,7 +12,6 @@ class Page:
         if self.num_records >=512:
             return False
         return True
-    
 
     def write(self, value):
         self.num_records += 1
--- lstore/table.py
+++ lstore/table.py
@@ -9,7 +9,7 @@ SCHEMA_ENCODING_COLUMN = 3
 
 class Record:
 
-    def __init__(self, rid, key, columns):
+    def __init__(self, rid, key, columns, data):
         self.rid = rid
         self.key = key
         self.columns = columns
